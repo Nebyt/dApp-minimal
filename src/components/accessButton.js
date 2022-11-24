20ca599fbc11bcbde56b6bf6f43f0363d6c85893
Denis <nebytov.d@gmail.com>
2022-11-24 14:23:37 +0300

    removed authentication

--- src/components/accessButton.js
+++ src/components/accessButton.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import useYoroi from '../hooks/yoroiProvider';
-import { textPartFromWalletChecksumImagePart } from '@emurgo/cip4-js';
 
 const AccessButton = () => {
   const { api, connect } = useYoroi();
-  const getWalletPlate = (apiObject) => {
-    const auth = apiObject.experimental.auth && apiObject.experimental.auth();
-    const walletId = auth.getWalletId();
-    return textPartFromWalletChecksumImagePart(walletId);
-  };
 
   return (
     <div className="mx-auto bg-gray-200">
@@ -16,11 +10,11 @@ const AccessButton = () => {
         {api ?
           <div className="text-xl font-bold tracking-tight text-black">
             Connected To Yoroi
-            <div className="py-1 text-xl font-bold tracking-tight text-black text-center">
-              {getWalletPlate(api)}
-            </div>
           </div> :
-          <button className="rounded-md border-black-300 bg-sky-600 hover:bg-sky-800 py-5 px-5 text-white font-bold" onClick={() => connect(true, false)}>
+          <button
+            className="rounded-md border-black-300 bg-sky-600 hover:bg-sky-800 py-5 px-5 text-white font-bold"
+            onClick={() => connect(false, false)}
+          >
             Request Access To Yoroi
           </button>
         }
